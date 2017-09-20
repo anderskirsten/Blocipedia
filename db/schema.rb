b493e079bfad44cafa7df3ec1d31016a67b38ea9
Kirsten Anderson <anders.kirsten@gmail.com>
2017-09-20 21:44:23 +0000

    User Roles: Complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920185250) do
+ActiveRecord::Schema.define(version: 20170920205649) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,11 +29,13 @@ ActiveRecord::Schema.define(version: 20170920185250) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.integer  "role"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["role"], name: "index_users_on_role"
 
   create_table "wikis", force: :cascade do |t|
     t.string   "title"
