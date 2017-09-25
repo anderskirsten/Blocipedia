d9417a9717cdf85c88efee2f0fbeb6af896fa2f2
Kirsten Anderson <anders.kirsten@gmail.com>
2017-09-25 15:55:30 +0000

    Private Wikis Feature: Complete

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -3,11 +3,17 @@ class DowngradeController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @user_wikis = @user.wikis.where(private: true)
     
-    refund = Stripe::Refund.create( charge: current_user.charge_id )
+    refund = Stripe::Refund.create( charge: @user.charge_id )
     
     if refund.status == 'succeeded'
-        current_user.update(role: 0)
+        @user.standard!
+        
+        @user_wikis.each do |wiki|
+          wiki.update_attribute(:private, false)
+        end
 
         flash[:notice] = "Your account has been downgraded to Standard Membership. Your private wikis are now public and you will recieve a $15.00 refund."
         redirect_to new_charge_path
